748d1433113ec6b17612cee527529a0550abb205
ElizaAugusta4 <elizaaugusta71@gmail.com>
2025-06-01 05:30:26 -0300

    Inicializando projeto com arquivos de infra

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "terraform-iac-versionnn"
-    key            = "terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
+    bucket  = "terraform-iac-versionnn"
+    key     = "terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
   }
 }
 
